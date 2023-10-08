51192ecb6a971c0b19dbce5e6676761b3fc03c0c
cuiyunhao <cuiyunhao@123.com>
2023-10-08 16:22:25 +0800

    feat: left完成

--- src/views/complaintIndicators/index.vue
+++ src/views/complaintIndicators/index.vue
@@ -9,43 +9,83 @@
             <span class="item-dec-num">100.11</span>
             件
           </p>
-          <p class="item-ranking">第一名</p>
+          <p class="item-ranking">第1名</p>
         </div>
         <div class="top-item">
-          <p class="item-dec">每网点投诉</p>
+          <p class="item-dec">每百万客户投诉</p>
           <p class="item-dec-piece">
-            <span class="item-dec-num">100.11</span>
+            <span class="item-dec-num">2000.23</span>
             件
           </p>
-          <p class="item-ranking">第一名</p>
+          <p class="item-ranking" style="color: #b5d9ff">第12名</p>
         </div>
         <div class="top-item">
-          <p class="item-dec">每网点投诉</p>
+          <p class="item-dec">每百亿资产投诉</p>
           <p class="item-dec-piece">
-            <span class="item-dec-num">100.11</span>
+            <span class="item-dec-num">1200.11</span>
             件
           </p>
-          <p class="item-ranking">第一名</p>
+          <p class="item-ranking" style="color: #fb3f22">第41名</p>
         </div>
       </div>
     </div>
 
-    <img :src="enp_kanban2" alt="" />
-    <img :src="enp_kanban1" alt="" />
+    <div class="center-area">
+      <div class="center-item">
+        <p class="center-title">银保监转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">20000</span>件</p>
+        </div>
+      </div>
+      <div class="center-item">
+        <p class="center-title">人行转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">20000</span>件</p>
+        </div>
+      </div>
+      <div class="center-item">
+        <p class="center-title">人行转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">23.83</span>%</p>
+          <p class="center-ranking" style="color: #fb3f22">第41名</p>
+        </div>
+      </div>
+      <div class="center-item">
+        <p class="center-title">人行转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">23.83</span>%</p>
+          <p class="center-ranking">第4名</p>
+        </div>
+      </div>
+      <div class="center-item">
+        <p class="center-title">人行转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">20000</span>件</p>
+        </div>
+      </div>
+      <div class="center-item">
+        <p class="center-title">人行转办投诉</p>
+        <div class="center-content">
+          <p class="center-num-dec"><span class="center-num">23.83</span>%</p>
+          <p class="center-ranking">第4名</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom-area">
+      <p class="bottom-title">客户满意度</p>
+      <div class="bottom-content">
+        <p class="bottom-num">99.98%</p>
+        <p class="bottom-ranking">第2名</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import enp_kanban0 from "@/assets/image/enp_kanban0.png";
-import enp_kanban1 from "@/assets/image/enp_kanban1.png";
-import enp_kanban2 from "@/assets/image/enp-kanban2.png";
 export default {
   data() {
-    return {
-      enp_kanban0,
-      enp_kanban1,
-      enp_kanban2,
-    };
+    return {};
   },
   created() {},
   mounted() {},
@@ -85,9 +125,9 @@ export default {
       justify-content: center;
       align-content: center;
       margin-top: 30px;
-      flex: 1;
 
       .top-item {
+        margin: 0 20px 0 20px;
         .item-dec {
           font-family: Microsoft YaHei;
           font-size: 12px;
@@ -104,6 +144,7 @@ export default {
           line-height: 22px;
           letter-spacing: 0em;
           text-align: left;
+          margin: 5px 0 5px 0;
 
           .item-dec-num {
             font-family: Microsoft YaHei;
@@ -113,8 +154,128 @@ export default {
             letter-spacing: 0em;
             text-align: left;
             text-shadow: 0px 0px 20px #257cff;
+            margin-right: 5px;
           }
         }
+
+        .item-ranking {
+          //styleName: 正文/加粗;
+          font-family: Microsoft YaHei;
+          font-size: 14px;
+          font-weight: 700;
+          line-height: 22px;
+          letter-spacing: 0em;
+          text-align: center;
+          color: #09cea9;
+        }
+      }
+    }
+  }
+
+  .center-area {
+    width: 362px;
+    display: flex;
+    flex-wrap: wrap;
+
+    .center-item {
+      width: 177px;
+      height: 106px;
+      border-radius: 1px;
+      background: url("../../assets/image/enp-kanban2.png");
+      background-size: contain;
+      margin-right: 8px;
+      margin: 5px 8px 5px 0;
+      &:nth-child(2n) {
+        margin-right: 0;
+      }
+
+      .center-title {
+        font-family: Microsoft YaHei;
+        font-size: 12px;
+        font-weight: 700;
+        line-height: 20px;
+        letter-spacing: 0em;
+        text-align: center;
+        color: #f0f6ff;
+        text-align: left;
+        position: relative;
+        top: 3px;
+        margin-left: 10px;
+      }
+
+      .center-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+        width: 145px;
+        height: 70px;
+        margin: 10px 0 0 15px;
+
+        .center-num-dec {
+          font-size: 14px;
+          font-weight: 700;
+          color: #ffffff;
+          .center-num {
+            font-family: Microsoft YaHei;
+            font-size: 16px;
+            font-weight: 700;
+            line-height: 28px;
+            letter-spacing: 0em;
+            text-align: center;
+            text-shadow: 0px 0px 20px #257cff;
+            margin-right: 5px;
+          }
+        }
+
+        .center-ranking {
+          font-family: Microsoft YaHei;
+          font-size: 14px;
+          font-weight: 700;
+          line-height: 22px;
+          letter-spacing: 0em;
+          text-align: center;
+          color: #09cea9;
+        }
+      }
+    }
+  }
+
+  .bottom-area {
+    width: 362px;
+    height: 80px;
+    background: url("../../assets/image/enp_kanban1.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+
+    .bottom-title {
+      font-family: Microsoft YaHei;
+      font-size: 12px;
+      font-weight: 700;
+      line-height: 20px;
+      letter-spacing: 0em;
+      text-align: left;
+      position: relative;
+      top: 6px;
+      margin-left: 10px;
+    }
+
+    .bottom-content {
+      display: flex;
+      margin: 18px 0 0 130px;
+      font-family: Microsoft YaHei;
+      font-size: 16px;
+      font-weight: 700;
+      line-height: 24px;
+      letter-spacing: 0em;
+      text-align: left;
+
+      .bottom-num {
+        margin-right: 65px;
+      }
+
+      .bottom-ranking {
+        color: #09cea9;
       }
     }
   }
