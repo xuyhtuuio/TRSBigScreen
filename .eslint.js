2bece117b74ac0a968d4ddb329da410a3f057aee
nimeimix <huo.linchun@trs.com.cn>
2023-10-09 12:31:28 +0800

    底部

---
+++ .eslint.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    root: true,
+    parserOptions: {
+      "ecmaVersion": 7,
+      sourceType: 'module'
+    },
+    "parser": "vue-eslint-parser",   // 添加这一句
+    rules: {
+      "@typescript-eslint/explicit-module-boundary-types":"off"
+    }
+  }
